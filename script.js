0b406e006076b1906abea8e9292175b073dbafa6
Jayesh Sharma <jayeshsh1707@gmail.com>
2023-06-13 14:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your code here
 function countLetters (){
-	const input = document.getElementbyId("evaluatedText").value;
+	const input = document.getElementById("evaluatedText").value;
 	const letterCount = input.length;
 	document.getElementById ("letterCount").textContent = letterCount
 }
 
-//your code here
-// function countLetters() {
+// //your code here
+// function countLetters(){
 //     const input = document.getElementById("evaluatedText").value;
 //     const letterCount = input.length;
 //     document.getElementById("letterCount").textContent = letterCount;
